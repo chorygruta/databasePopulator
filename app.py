d489babaf2640db8378559ad56be4c2fa5b38d66
Chory Gruta <chorygruta@gmail.com>
2017-08-18 13:17:23 -0700

    added sql on postgres

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ README: Step 1: Change apiKey variable with your key
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'thisisasecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres://Kookinwats@ec2-54-163-237-25.compute-1.amazonaws.com:5432/d6nnmpc09qqr2n'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fctntwrmfooapf:8897d30c490ecfbd4182e5e8a60966b57f6cd0557ee2ed4c880463e9619189f4@ec2-54-163-237-25.compute-1.amazonaws.com:5432/d6nnmpc09qqr2n'
 #app.config['SERVER_NAME'] = 'localhost:5000'
 
 heroku = Heroku(app)
